67295a9bbac80cbf3afe16bc1ee317795485cdf4
Green Lera <zelnayavaleriya@gmail.com>
2016-10-06 16:20:57 +0300

    Доработка задания #6

--- src/js/check.js
+++ src/js/check.js
@@ -1,17 +1,57 @@
 function getMessage(a, b) {
 
-  if (typeof a == "boolean" && a == true) {
+  var amountOfRedPoints = 0;
+  var artifactsSquare = 0;
+
+  if (a === true) {
+
     return ("Переданное GIF-изображение анимировано и содержит " + [b] + " кадров");
-  } else if (typeof a == "boolean" && a == false) {
+
+  } 
+
+  if (a === false) {
+
     return ("Переданное GIF-изображение не анимировано");
-  } else if (typeof a == "number") {
+
+  } 
+
+  if (typeof a === "number") {
+
     return ("Переданное SVG-изображение содержит " + [a] + " объектов и " + [b * 4] + " атрибутов");
-  } else if (typeof a == "object") {
+
+  } 
+
+  if (a instanceof Array && !(b instanceof Array)) {
+
+    for ( var i = 0; i < a.length; i++ ) {
+
+    amountOfRedPoints += a[i];
+
+    }
+
     return ("Количество красных точек во всех строчках изображения: " + [amountOfRedPoints]);
-  } else if (typeof a == "object" && typeof b == "object") {
+  } 
+
+  if (a instanceof Array && b instanceof Array) {
+
+    var minLength = (a.length < b.length)? a.length : b.length; 
+
+    for ( var i = 0; i < minLength; i++ ) {
+
+    artifactsSquare += a[i] * b[i];
+
+    }
+
     return ("Общая площадь артефактов сжатия: " + [artifactsSquare] +  " пикселей");
-  } else {
-    return ("Переданы некорректные данные"); 
-  }
+    
+  } 
+
+
+  return ("Переданы некорректные данные"); 
 
 }
+
+
+
+
+
