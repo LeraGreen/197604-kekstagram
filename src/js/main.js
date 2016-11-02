9c2790b6e1e253f36de32c77ef7502c9e2dd3166
Green Lera <zelnayavaleriya@gmail.com>
2016-11-02 22:25:11 +0300

    Модули AMD. Изменения в main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 'use strict';
 
-define(['./pictures.js'], function(pictures) {});
-define(['./upload.js'], function(upload) {});
-define(['./resizer.js'], function(resizer) {});
+define(['./pictures.js'], function(pictures) {return pictures});
+define(['./upload.js'], function(upload) {return upload});
+define(['./resizer.js'], function(resizer) {return resizer});
