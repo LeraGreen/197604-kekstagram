2e570217bb013f4ef9be2970a56730d2ca7611be
Green Lera <zelnayavaleriya@gmail.com>
2016-11-03 10:41:28 +0300

    Убрала ошибки

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,13 @@
 'use strict';
 
-define(['./pictures.js'], function(pictures) {return pictures});
-define(['./upload.js'], function(upload) {return upload});
-define(['./resizer.js'], function(resizer) {return resizer});
+define(['./pictures.js'], function(pictures) {
+  return pictures;
+});
+
+define(['./upload.js'], function(upload) {
+  return upload;
+});
+
+define(['./resizer.js'], function(resizer) {
+  return resizer;
+});
