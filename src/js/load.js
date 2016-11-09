914acafa8586b6576b945253805e778c870c3c77
Green Lera <zelnayavaleriya@gmail.com>
2016-11-09 21:27:40 +0300

    события

--- src/js/load.js
+++ src/js/load.js
@@ -1,14 +1,33 @@
 'use strict';
 
 define(function() {
-  function createCallBack(url, callback) {
-    var script = document.createElement('script');
-    script.src = url;
-    document.body.appendChild(script);
 
-    window.JSONPCallback = function(data) {
+  function getSearchSrting(params) {
+    return Object.keys(params).map(function(param) {
+      return [param, params[param]].join('=');
+    }).join('&');
+  }
+
+  function createCallBack (url, params, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function(evt) {
+      var data = JSON.parse(evt.target.response);
       callback(data);
     };
-  }
+    xhr.open('GET', url + '?' + getSearchSrting(params));
+    xhr.send();
+  };
+
   return createCallBack;
+
+  //function createCallBack(url, callback) {
+    //var script = document.createElement('script');
+    //script.src = url;
+    //document.body.appendChild(script);
+
+    //window.JSONPCallback = function(data) {
+    //  callback(data);
+    //};
+  //}
+  //return createCallBack;
 });
--- src/js/pictures.js
+++ src/js/pictures.js
@@ -11,19 +11,53 @@ define(['./load.js', './gallery.js'],
       hideFilters();
       gallery.setPictures(arr);
       gallery.render();
-      // gallery.show(0);
 
       if (typeof callback === 'function') {
         callback();
       }
     }
 
-    load('http://localhost:1507/api/pictures?callback=JSONPCallback', function(data) {
+    var pageNumber = 0;
+    var pageSize = 12;
+    var activeFilter = 'filter-popular';
+    var footer = document.querySelector('footer');
+
+    function changeFilter(filterID) {
+      document.querySelector('.pictures').innerHtml = '';
+      activeFilter = filterID;
+      pageNumber = 0;
+      loadPhotos(filterID, pageNumber);
+    }
+
+    var lastCall = new Date();
+
+    window.addEventListener('scroll', function() {
+      if (footer.getBoundingClientRect().bottom - window.innerHeight <= 100) {
+        loadPhotos(activeFilter, ++pageNumber);
+      }
+    });
+
+    document.querySelector('.filters').addEventListener('change', function(evt){
+      if (evt.target.classList.contains('filters-radio')) {
+        changeFilter(evt.target.id);
+      }
+    });
+
+    function loadPhotos(filter, currentPage) {
+      load('/api/pictures',{
+        from: currentPage * pageSize,
+        to: (currentPage * pageSize) + pageSize
+        //filter: filter
+      },
+      function(data) {
       var pictures = data;
       addPictureBlock(pictures, function() {
         document.querySelector('.filters').classList.remove('hidden');
       });
     });
+    }
+
+    changeFilter(activeFilter);
   });
 
 
